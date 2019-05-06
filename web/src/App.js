bfad83ac4ad592254be264d88ccb3a39f7d03345
Carlo Cabello <CCabello@linz.govt.nz>
2019-05-06 16:28:59 +1200

    test change

--- web/src/App.js
+++ web/src/App.js
@@ -30,6 +30,7 @@ function Home() {
       <div style={{position: "fixed", top: "40%", left: "50%", transform: "translate(-50%, -50%)", border: "1px solid #dee2e6", padding: "50px"}}>
         <h3>Welcome to LandOnLite</h3>
         <p>You can enter a title number (e.g. "1") to view it.</p>
+        <h3>hello</h3>
         <TitleSearch />
       </div>
     </div>
